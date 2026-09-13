23da4b6f1adc642522bda4d1655df54e6e4de7b1
hsrvms <59848940+hsrvms@users.noreply.github.com>
2026-09-13 13:14:37 +0300

    fix: keep space independent in gaming layer

--- keyboards/crkbd/rev1/keymaps/miryoku/config.h
+++ keyboards/crkbd/rev1/keymaps/miryoku/config.h
@@ -31,7 +31,7 @@ LAYOUT_split_3x6_3( \
  KC_TAB, K00, K01, K02, K03, K04,             K05, K06, K07, K08, K09, KC_BSLS, \
  KC_LSFT,K10, K11, K12, K13, K14,             K15, K16, K17, K18, K19, KC_QUOT, \
  KC_LCTL,K20, K21, K22, K23, K24,             K25, K26, K27, K28, K29, GAME_TOGGLE, \
-                         KC_LALT, LT(U_GAME_FN, KC_SPC), KC_M, K35, K36, K37 \
+                         LT(U_GAME_FN, KC_LALT), KC_SPC, KC_M, K35, K36, K37 \
 )
 
 #define LAYOUT_miryoku_game_fn( \
--- keyboards/crkbd/rev1/keymaps/miryoku/manna-harbour_miryoku.h
+++ keyboards/crkbd/rev1/keymaps/miryoku/manna-harbour_miryoku.h
@@ -31,7 +31,7 @@ MIRYOKU_LAYER_LIST
 KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
 KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,           \
 KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,           KC_SLSH,           \
-U_NP,              U_NP,              KC_LALT,           LT(U_GAME_FN,KC_SPC), KC_M,             LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
+U_NP,              U_NP,              LT(U_GAME_FN,KC_LALT), KC_SPC,           KC_M,             LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
 
 #define MIRYOKU_GAME_FN_LAYER \
 KC_1,              KC_TRNS,           KC_2,              KC_3,              KC_4,              KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,           \
